0097fa45e4f58966393a1a9eba58afa904572762
NienkeUijlen <n.m.uijlen@students.uu.nl>
2017-10-20 13:28:17 +0200

    Test

--- syllabification.py
+++ syllabification.py
@@ -24,7 +24,7 @@ def InitialSplitWordOnSyllable(word):
     for i in range(2, len(vowelPositions) + 1):
         outputWord = outputWord[0:vowelPositions[-i] + 2] + " " + outputWord[vowelPositions[-i] + 2:]
 
-    return outputWord
+    return outputWord#fjgskjdk
 
 # illegal letter combinations (pm, lf, lm)
 def fixIllegalCombinationsSyllable(input, firstLetter, secondLetter):
